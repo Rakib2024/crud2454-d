b2f918a9a38ec65db1213a811dfb8c76ea124fbb
Rakib2024 <iamrakib0000@gmail.com>
2023-03-01 15:33:29 +0600

    deleted 2 files from my project and changed the server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 const connectDB = require('./server/database/connection');
 
 const app = express();
+// ashim
 
 dotenv.config( { path : '.env'} )
 const PORT = process.env.PORT || 8080
